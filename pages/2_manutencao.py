3a34c105bcad1c2c61b824343d13351591ed2989
Filipe Ambrozio <filipe.ambrozio.s3@gmail.com>
2026-03-23 17:05:45 -0300

    Implement login check and vehicle ID management
    Added login check and updated vehicle ID handling.

--- pages/2_manutencao.py
+++ pages/2_manutencao.py
@@ -3,6 +3,10 @@ from supabase_config import supabase
 from datetime import date
 import pandas as pd
 
+if "logado" not in st.session_state or not st.session_state.logado:
+    st.warning("Faça login primeiro")
+    st.stop()
+
 st.title("🔧 Manutenção")
 
 tipos = [
@@ -42,7 +46,7 @@ veiculos = supabase.table("veiculos").select("*").execute().data
 
 lista = [v["nome"] for v in veiculos]
 
-veiculo = st.selectbox("Veículo", lista)
+veiculo_id = st.session_state["veiculo_id"]
 tipo = st.selectbox("Tipo de manutenção", tipos)
 
 
@@ -51,13 +55,14 @@ data_inicial = st.date_input("Data inicial")
 periodo = st.number_input("Periodo em meses", value=6)
 valor = st.number_input("Valor")
 intervalo_km = st.number_input("intervalo_km", value=60079.00)
+km_atual = st.number_input("KM Atual", value=60079.0)
 
 if st.button("Salvar manutenção"):
 
-    id_veiculo = [v["id"] for v in veiculos if v["nome"] == veiculo][0]
+    id_veiculo = st.session_state.veiculo_id
 
     supabase.table("manutencoes").insert({
-        "veiculo_id": id_veiculo,
+        "veiculo_id": veiculo_id,
         "tipo_manutencao": tipo,
         "km_atual": km_atual,
         "km_inicial": km_inicial,
