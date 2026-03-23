db7385dd887bbd712e0e9b407be8932b14151954
Filipe Ambrozio <filipe.ambrozio.s3@gmail.com>
2026-03-23 17:04:58 -0300

    Add login functionality and session management- atualização

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from supabase_config import supabase
 from datetime import datetime
 
 st.set_page_config(
@@ -6,27 +7,81 @@ st.set_page_config(
     layout="centered"
 )
 
-st.title("🚗 Controle Veicular Profissional")
+# Inicializar sessão
+if "logado" not in st.session_state:
+    st.session_state.logado = False
 
-st.markdown("""
-### Sistema desenvolvido para:
+if "veiculo_id" not in st.session_state:
+    st.session_state.veiculo_id = None
 
-✅ Controle de veículos  
-✅ Histórico de manutenção  
-✅ Controle de combustível  
-✅ Alertas de troca por KM e tempo  
-✅ Organização financeira automotiva  
+if "veiculo_nome" not in st.session_state:
+    st.session_state.veiculo_nome = None
 
-Permite acompanhar custos, prevenir atrasos de manutenção e manter o veículo em dia.
-""")
 
-st.divider()
+# LOGIN
+if not st.session_state.logado:
 
-hoje = datetime.now().strftime("%d/%m/%Y")
+    st.title("🔐 Login Veicular")
 
-st.info(f"📅 Hoje: {hoje}")
+    veiculos = supabase.table("veiculos").select("*").execute().data
 
-st.warning("""
-### 💰 Lembrete IPVA
-Verifique o calendário do IPVA do seu estado para evitar juros e atraso.
-""")
+    lista = [v["nome"] for v in veiculos]
+
+    veiculo = st.selectbox("Veículo", lista)
+    senha = st.text_input("Senha", type="password")
+
+    if st.button("Entrar"):
+
+        usuario = [
+            v for v in veiculos
+            if v["nome"] == veiculo and str(v["senha"]) == str(senha)
+        ]
+
+        if usuario:
+            st.session_state.logado = True
+            st.session_state.veiculo_id = usuario[0]["id"]
+            st.session_state.veiculo_nome = usuario[0]["nome"]
+            st.rerun()
+        else:
+            st.error("Senha incorreta")
+
+
+# SISTEMA
+else:
+
+    st.title("🚗 Controle Veicular")
+    
+    st.markdown("""
+    ### Sistema desenvolvido para:
+
+    ✅ Controle de veículos  
+    ✅ Histórico de manutenção  
+    ✅ Controle de combustível  
+    ✅ Alertas de troca por KM e tempo  
+    ✅ Organização financeira automotiva  
+
+    Permite acompanhar custos, prevenir atrasos de manutenção e manter o veículo em dia.
+    """)
+
+    st.divider()
+
+    hoje = datetime.now().strftime("%d/%m/%Y")
+
+    st.info(f"📅 Hoje: {hoje}")
+
+    st.warning("""
+    ### 💰 Lembrete IPVA
+    Verifique o calendário do IPVA do seu estado para evitar juros e atraso.
+    """)    
+
+    # hoje = datetime.now().strftime("%d/%m/%Y")
+
+    st.success(f"Veículo: {st.session_state.veiculo_nome}")
+    # st.write(f"📅 Data: {hoje}")
+    # st.warning("💰 Lembrete IPVA")
+
+    if st.button("Sair"):
+        st.session_state.logado = False
+        st.session_state.veiculo_id = None
+        st.session_state.veiculo_nome = None
+        st.rerun()
